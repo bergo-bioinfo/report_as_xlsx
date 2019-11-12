30b7afdc5c60ddda913c864de4921494d37fb63e
Quentin_ <qcavaille@ZBook-PM>
2019-11-12 15:38:44 +0100

    Add mandatory files for REDCap module

---
+++ ReportAsXlsx.php
@@ -0,0 +1,10 @@
+<?php
+namespace Bergonie\ReportAsXlsx;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+class ReportAsXlsx extends AbstractExternalModule{
+
+}
+?>
